5ec6d86f4e8242b8147a4f801badd0a4a81e3240
sagarrajput91 <sagar.rajput91@gmail.com>
2021-12-29 11:16:30 +0530

    Changed stage name for plankey1 --> COMMITTED2

--- src/bamboo-specs/PlanSpec.java
+++ src/bamboo-specs/PlanSpec.java
@@ -12,7 +12,7 @@ public class PlanSpec {
                 new BambooKey("PLANKEY1"))
                 .description("hello")
                 .pluginConfigurations(new ConcurrentBuilds())
-                .stages(new Stage("COMMITTED1")
+                .stages(new Stage("COMMITTED2")
                         .jobs(new Job("Job Name",
                                 new BambooKey("JOBKEY"))
                                 .artifacts(new Artifact()
